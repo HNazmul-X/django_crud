34525a8fa6918ce11864dd94f5083fc4e5f97cdb
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-10-19 12:17:12 +0600

    url.py <employee> updated

--- employe/urls.py
+++ employe/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.employee_form, name="employee_form"),
     path("list/", views.employee_list, name="employee_list"),
     path("edit/<slug:id>", views.employee_update,name="employee_update"),
-    path("/delete/<slug:id>", views.deleteEmployee,name="delete_employe")
+    path("delete/<slug:id>", views.deleteEmployee,name="delete_employe")
 ]
